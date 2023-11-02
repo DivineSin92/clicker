290f9b87835932eb91eefe16e3d115fc476ad939
Michał Mazur <147649867+DivineSin92@users.noreply.github.com>
2023-11-02 22:04:27 +0100

    Update clicker.py

--- clicker.py
+++ clicker.py
@@ -1,6 +1,14 @@
 from pyautogui import *
 import pyautogui
 import time
+import requests
+
+def message():
+    token = ''
+    chat_id = ''
+    url = f'https://api.telegram.org/bot{token}/sendMessage'
+    parms = {'chat_id': chat_id, 'text': 'come back'}
+    requests.get(url, params = parms)
 
 def click_img():
     location = pyautogui.locateOnScreen('click.png', confidence = 0.75)
@@ -9,6 +17,7 @@ def click_img():
 def is_on_screen():
     if pyautogui.locateOnScreen('click.png', confidence = 0.75) != None:
         click_img()
+        message()
         return False
     else:
         time.sleep(0.5)
